c2eda6fce8f2db178b31131842c461f2f1b56a56
ashleymader <ashley.mader12@gmail.com>
2020-11-10 14:09:48 -0500

    validations for login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
     has_many :reviews 
     has_many :resources #they created 
-    has_many :reviewed_resources, through: :reviews. source: :resource #reviews on OTHER peoples resources 
+    has_many :reviewed_resources, through: :reviews, source: :resource #reviews on OTHER peoples resources 
     #needed to use source to distinguish naming convention. Cannot have 2 has_many relationships with the same name
+    
+    validates :username, uniqueness: true,  presence: true
+    validates :email, uniqueness: true, presence: true
+    has_secure_password
 end
