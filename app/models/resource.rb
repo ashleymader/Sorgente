9ebfeb87bc2e1e1dc7bd512a5d2601fdc8b2691a
ashleymader <ashley.mader12@gmail.com>
2020-11-10 13:51:19 -0500

    associations

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,6 @@
 class Resource < ApplicationRecord
-  belongs_to :user
+  belongs_to :user #the user who added the resource
   belongs_to :topic
+  has_many :reviews 
+  has_many :users, through: :reviews #users who reviewed the resource 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :reviews 
+    has_many :resources #they created 
+    has_many :reviewed_resources, through: :reviews. source: :resource #reviews on OTHER peoples resources 
+    #needed to use source to distinguish naming convention. Cannot have 2 has_many relationships with the same name
 end
