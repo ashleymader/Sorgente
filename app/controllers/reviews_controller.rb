97d3e57cf9b7b178b381bc5f1bf2fdfb8f2a9195
ashleymader <ashley.mader12@gmail.com>
2020-11-11 13:39:37 -0500

    views for reviews/new/index controller actions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,10 @@
 class ReviewsController < ApplicationController
+
+    def index 
+        @reviews = Review.all 
+    end
+
+    def new 
+        @review = Review.new(review_params)
+    end
 end
