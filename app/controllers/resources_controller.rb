368430d241c6f63ec11d5d41651214ff7cd8675c
ashleymader <ashley.mader12@gmail.com>
2020-11-11 13:36:22 -0500

    create/show resource working with strong params

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,2 +1,31 @@
 class ResourcesController < ApplicationController
+
+
+    def new 
+        @resource = Resource.new 
+        @resource.build_topic 
+    end
+
+    def create 
+        @resource = Resource.new(resource_params)
+        @resource.user_id = session[:user_id]
+        if @resource.save
+            redirect_to resource_path(@resource)
+        else  
+            @resource.build_topic
+            render :new
+        end
+    end
+
+    def show
+        @resource = Resource.find_by(params[:id])
+    end
+
+    private 
+
+    def resource_params 
+        params.require(:resource).permit(:url, :description, :topic_id, topic_attributes: [:name])
+    end
+
+
 end
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,4 +3,12 @@ class Resource < ApplicationRecord
   belongs_to :topic
   has_many :reviews 
   has_many :users, through: :reviews #users who reviewed the resource 
+
+  
+  def topic_attributes=(attributes)
+    self.topic = Topic.find_or_create_by(attributes) if !attributes['name'].empty?
+    self.topic
+  end
+
+
 end
