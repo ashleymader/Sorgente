45926132aa519e8c85a26d6b904f01a9ae787fa8
ashleymader <ashley.mader12@gmail.com>
2020-11-11 13:34:00 -0500

    cleanup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
     def destroy 
         session.delete(:user_id)
-        redirect_to '/'
+        redirect_to root_path
     end
 
     def new 
@@ -15,12 +15,12 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by(username: params[:user][:username])
-        if @user.try(:authenticate, params[:user][:password]) #works the same as @user && @user.authenticate(params[:user][:password])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else  
             flash[:error] = "Please try again"
             redirect_to login_path
         end
-
     end
 end
