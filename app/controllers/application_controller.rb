624e66fff39becc1016d84a86b45c60c9f4687af
ashleymader <ashley.mader12@gmail.com>
2020-11-12 12:50:19 -0500

    helper for current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+
+    def current_user 
+        @current_user || User.find_by_id(session[:user_id])
+        #limits calls to db by seeing if a current user is set, if not looks inside the sessions hash to find user_id and sets the currentuser. 
+    end
 end
