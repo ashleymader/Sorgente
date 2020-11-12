8106fc9c0f250649fb06e6e64abbcf71d7853bce
ashleymader <ashley.mader12@gmail.com>
2020-11-12 12:52:08 -0500

    topic index/show working

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,8 +1,12 @@
 class TopicsController < ApplicationController
 
     def index
-        @topics = Topic.all
+        @topics = Topic.all 
     end
 
+    def show
+        @topic = Topic.find_by_id(params[:id])
+        @resources = @topic.resources
+    end
 
 end
