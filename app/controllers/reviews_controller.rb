e9017c88f82dbba68986ef2e1d3e244a5265bcfb
ashleymader <ashley.mader12@gmail.com>
2020-11-12 12:54:48 -0500

    form for review/create, , new, index, show action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,35 @@
 class ReviewsController < ApplicationController
 
     def index 
-        @reviews = Review.all 
+        if @resource = Resource.find_by_id(params[:resource_id])
+            @reviews = @resource.reviews
+            binding.pry
+        else  
+            @reviews = Review.all 
+        end
     end
 
     def new 
-        @review = Review.new(review_params)
+        @resource = Resource.find_by_id(params[:resource_id])
+        @review = @resource.reviews.build
+    end
+
+    def create 
+        @review = current_user.reviews.build(review_params)
+        if @review.save
+            redirect_to review_path(@review)
+        else  
+            render :new
+        end
+    end
+
+    def show 
+        @review = Review.find_by_id(params[:id])
+    end
+
+    private 
+
+    def review_params 
+        params.require(:review).permit(:resource_id, :stars, :comment, :title )
     end
 end
