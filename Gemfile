1319155625e64adad5e0bc54494ac67f373abeed
ashleymader <ashley.mader12@gmail.com>
2020-11-18 10:39:17 -0500

    seed data and gems

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'pry'
+  gem 'faker'
 end
 
 group :development do
@@ -41,14 +42,18 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'faker'
 end
 
 group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
+  gem 'faker'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
+  gem 'database_cleaner-active_record'
+  gem 'database_cleaner-redis'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,88 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'database_cleaner/active_record'
+require 'faker'
+DatabaseCleaner.clean_with(:truncation)
+
+#--users--#
+    user_1 = User.create(
+        username: "ashley", 
+        email: "ashley@ashley.com",
+        password: "pw")
+
+    user_2 = User.create(
+        username: "nick", 
+        email: "nick@nick.com",
+        password: "pw")
+
+    user_3 = User.create(
+        username: "dustin", 
+        email: "dustin@dustin.com",
+        password: "pw")
+
+#--topics--#
+    topic_1 = Topic.create(name: Faker::ProgrammingLanguage.name)
+
+    topic_2 = Topic.create(name: Faker::ProgrammingLanguage.name)
+
+    topic_3 = Topic.create(name: Faker::ProgrammingLanguage.name)
+
+    topic_4 = Topic.create(name: Faker::ProgrammingLanguage.name)
+
+
+#--resources--#
+    resource_1 = Resource.create(url: Faker::Internet.url, 
+        description: "Resource 1 description", 
+        user_id: user_1.id, 
+        topic_id: topic_1.id)
+
+    resource_2 = Resource.create(url: Faker::Internet.url, 
+        description: "Resource 2* description", 
+        user_id: user_2.id, 
+        topic_id: topic_2.id)
+
+    resource_3 = Resource.create(url: Faker::Internet.url, 
+        description: "Resource 3! description", 
+        user_id: user_3.id, 
+        topic_id: topic_3.id)
+
+    resource_4 = Resource.create(url: Faker::Internet.url, 
+        description: "Resource !!!4 description", 
+        user_id: user_1.id, 
+        topic_id: topic_4.id)
+
+#--reviews--#
+
+    Review.create(stars: Faker::Number.between(from: 1, to: 5), 
+        title: "Wow", 
+        comment: "Best resource out there for Topic 2", 
+        resource_id: resource_2.id,
+        user_id: user_2.id )
+
+    Review.create(stars: Faker::Number.between(from: 1, to: 5), 
+        title: "Great", 
+        comment: "Best resource out there for Topic 3", 
+        resource_id: resource_3.id,
+        user_id: user_3.id )
+
+    Review.create(stars: Faker::Number.between(from: 1, to: 5), 
+        title: "So good", 
+        comment: "Best resource out there for Topic 1", 
+        resource_id: resource_1.id,
+        user_id: user_1.id )
+        
+    Review.create(stars: Faker::Number.between(from: 1, to: 5), 
+        title: "amazing", 
+        comment: "Best resource out there for Topic 4", 
+        resource_id: resource_4.id,
+        user_id: user_1.id )
+
+
+    
+
+
+
+
+
