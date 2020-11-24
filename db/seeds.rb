5d23506f12dcd8d81f02ae69595cb5fd583223dd
ashleymader <ashley.mader12@gmail.com>
2020-11-24 10:20:10 -0500

    got rid of some faker seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,63 +27,62 @@ DatabaseCleaner.clean_with(:truncation)
         password: "pw")
 
 #--topics--#
-    topic_1 = Topic.create(name: Faker::ProgrammingLanguage.name)
+    topic_1 = Topic.create(name: "SQL")
 
-    topic_2 = Topic.create(name: Faker::ProgrammingLanguage.name)
+    topic_2 = Topic.create(name: "Rails")
 
-    topic_3 = Topic.create(name: Faker::ProgrammingLanguage.name)
+    topic_3 = Topic.create(name: "GitHub")
 
-    topic_4 = Topic.create(name: Faker::ProgrammingLanguage.name)
-
+    topic_4 = Topic.create(name: "Javascript" )
 
 #--resources--#
-    resource_1 = Resource.create(site_name: "Site 1 Name", 
-        url: Faker::Internet.url, 
-        description: "Resource 1 description", 
+    resource_1 = Resource.create(site_name: "SQLClauses", 
+        url: "http://www.sqlclauses.com/sql+aggregate+functions", 
+        description: "SQL aggregate functions", 
         user_id: user_1.id, 
         topic_id: topic_1.id)
 
-    resource_2 = Resource.create(site_name: "Site 2 Name", 
-        url: Faker::Internet.url, 
-        description: "Resource 2* description", 
+    resource_2 = Resource.create(site_name: "Rails Notes For Professionals Book", 
+        url: "https://books.goalkicker.com/RubyOnRailsBook/", 
+        description: "Free book on rails compiled by the people at stack overflow", 
         user_id: user_2.id, 
         topic_id: topic_2.id)
 
-    resource_3 = Resource.create(site_name: "Site 3 Name", 
-        url: Faker::Internet.url, 
-        description: "Resource 3! description", 
+    resource_3 = Resource.create(site_name: "Oh Shit Git", 
+        url: "http://ohshitgit.com/", 
+        description: "What to do if you make a mistake. This isn't always addressed in beginner courses", 
         user_id: user_3.id, 
         topic_id: topic_3.id)
 
-    resource_4 = Resource.create(site_name: "Site 4 Name", 
-        url: Faker::Internet.url, 
-        description: "Resource !!!4 description", 
+    resource_4 = Resource.create(site_name: "Async JavaScript Cheatsheet", 
+        url: "https://raw.githubusercontent.com/frontarm/async-javascript-cheatsheet/0fc4ce398819501062a9be311d1fcf35ececa00c/async-cheatsheet.png", 
+        description: "Javascript cheat sheet", 
         user_id: user_1.id, 
         topic_id: topic_4.id)
 
 #--reviews--#
 
-    Review.create(stars: Faker::Number.between(from: 1, to: 5), 
+    Review.create(stars: "5", 
         title: "Wow", 
-        comment: "Best resource out there for Topic 2", 
+        comment: "Best resource out there for Rails", 
         resource_id: resource_2.id,
         user_id: user_2.id )
 
-    Review.create(stars: Faker::Number.between(from: 1, to: 5), 
+    Review.create(stars: "5", 
         title: "Great", 
-        comment: "Best resource out there for Topic 3", 
+        comment: "Helped me understand how to fix xyz mistake I made.", 
         resource_id: resource_3.id,
         user_id: user_3.id )
 
-    Review.create(stars: Faker::Number.between(from: 1, to: 5), 
-        title: "So good", 
-        comment: "Best resource out there for Topic 1", 
+    Review.create(stars: "2", 
+        title: "Bad", 
+        comment: "This site just didn't help at all. Too confusing.", 
         resource_id: resource_1.id,
         user_id: user_1.id )
         
-    Review.create(stars: Faker::Number.between(from: 1, to: 5), 
-        title: "amazing", 
-        comment: "Best resource out there for Topic 4", 
+    Review.create(stars: "4", 
+        title: "Love it!", 
+        comment: "Short and sweet cheatsheet!", 
         resource_id: resource_4.id,
         user_id: user_1.id )
 
